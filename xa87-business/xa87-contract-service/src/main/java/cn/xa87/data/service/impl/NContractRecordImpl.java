5629792d2e1671c5be91dcaf6d740c819843c881
huyaq <17358892623@163.com>
2023-03-28 00:17:14 +0800

    修改

--- xa87-business/xa87-contract-service/src/main/java/cn/xa87/data/service/impl/NContractRecordImpl.java
+++ xa87-business/xa87-contract-service/src/main/java/cn/xa87/data/service/impl/NContractRecordImpl.java
@@ -5,9 +5,11 @@ import cn.xa87.data.mapper.NContractRecordMapper;
 import cn.xa87.data.service.NContractRecordService;
 import cn.xa87.model.NContractRecord;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service
 public class NContractRecordImpl extends ServiceImpl<NContractRecordMapper,NContractRecord> implements NContractRecordService {
     @Resource
     BalanceMapper balanceMapper;
